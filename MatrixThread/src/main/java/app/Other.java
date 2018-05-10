14219546a67fb06b5b4823f7e24c02cebdb97ad3
unknown <chirilovici.janea@gmail.com>
2018-05-10 14:48:11 +0300

    Matrix

---
+++ MatrixThread/src/main/java/app/Other.java
@@ -0,0 +1,37 @@
+package app;
+
+//import static app.Main.getMatrix;
+
+public class Other extends Thread {
+
+    int A[][] = Main.matrix;
+//    int A[][] = getMatrix();
+    private int row;
+    private int column;
+
+    public Other(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public void run(){
+        int sum = sumElemet();
+        synchronized (Main.dog){
+            try {
+                Main.counter++;
+                Main.dog.wait();
+                A[row][column] = sum;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int sumElemet(){
+        int i =row,j=column;
+        return A[i-1][j-1]+A[i-1][j]+A[i-1][j+1]+
+                A[i][j-1]+A[i][j+1]+A[i][j]+
+                A[i+1][j-1]+A[i+1][j]+A[i+1][j+1];
+    }
+}
